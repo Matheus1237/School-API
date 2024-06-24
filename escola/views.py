fcd8bfd1cacbde416e81ced6be91adbe10529884
Matheus1237 <masian202207@icloud.com>
2024-06-24 11:50:29 -0300

    first commit

---
+++ escola/views.py
@@ -0,0 +1,18 @@
+from rest_framework import viewsets
+from escola.models import Aluno, Curso, Matricula
+from escola.serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer
+
+class AlunosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os alunos e alunas"""
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+
+class CursosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os cursos"""
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+
+class MatriculaViewSet(viewsets.ModelViewSet):
+    """Listando todas as matrículas"""
+    queryset = Matricula.objects.all()
+    serializer_class = MatriculaSerializer
